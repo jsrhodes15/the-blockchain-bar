8db20eed5182c9aaa72efbabb52a26967bc30c9d
Jordan Rhodes <jsrhodes15@gmail.com>
2020-09-20 13:08:51 -0400

    Initialize CLI program to interact with blockchain

---
+++ cmd/tbb/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var tbbCmd = &cobra.Command{
+		Use:   "tbb",
+		Short: "The Blockchain Bar CLI",
+		Run: func(cmd *cobra.Command, args []string) {
+
+		},
+	}
+
+	err := tbbCmd.Execute()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jsrhodes15/the-blockchain-bar
+
+go 1.14
+
+require github.com/spf13/cobra v1.0.0
