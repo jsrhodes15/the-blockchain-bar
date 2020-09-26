e2cd6bb0f8a52c2ec8b482b6b322c9b9f6a9ba5c
Jordan Rhodes <jsrhodes15@gmail.com>
2020-09-26 10:18:08 -0400

    Provide default data directory if no flag passed

--- cmd/tbb/main.go
+++ cmd/tbb/main.go
@@ -3,13 +3,24 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
 
 const flagDataDir = "datadir"
+const defaultDataDirname = ".tbb"
+var defaultDataDir string
 
 func main() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	defaultDataDir = filepath.Join(homeDir, defaultDataDirname)
+
 	var tbbCmd = &cobra.Command{
 		Use:   "tbb",
 		Short: "The Blockchain Bar CLI",
@@ -22,7 +33,7 @@ func main() {
 	tbbCmd.AddCommand(balancesCmd())
 	tbbCmd.AddCommand(txCmd())
 
-	err := tbbCmd.Execute()
+	err = tbbCmd.Execute()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
@@ -30,14 +41,13 @@ func main() {
 }
 
 func addDefaultRequiredFlags(cmd *cobra.Command) {
-	cmd.Flags().String(
+	cmd.Flags().StringP(
 		flagDataDir,
-		"",
+		"d",
+		defaultDataDir,
 		"Absolute path where tbb data is stored",
-		)
-	cmd.MarkFlagRequired(flagDataDir)
+	)
 }
-
 
 func incorrectUsageErr() error {
 	return fmt.Errorf("incorrect usage")
